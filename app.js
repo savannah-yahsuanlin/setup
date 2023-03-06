88e34e5088044cff07dea53142be6ed03d13f485
savannah-yahsuanlin <savannahwang75@gmail.com>
2023-03-06 19:05:04 +0800

    update

---
+++ app.js
@@ -0,0 +1,7 @@
+var add = function (num1, num2) {
+    return num1 + num2;
+};
+var one = '3';
+var two = 11;
+var result = add(one, two);
+console.log(result);
---
+++ app.ts
@@ -0,0 +1,9 @@
+const add = (num1: number, num2: number) => {
+	return num1 + num2;
+}
+
+const one = '3'
+const two = 11
+
+const result = add(one, two)
+console.log(result)
