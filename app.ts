a9dc2eb02b1f7eea0e0a2b785563d902011cf834
savannah-yahsuanlin <savannahwang75@gmail.com>
2023-03-08 20:19:21 +0800

    type alias

--- app.ts
+++ app.ts
@@ -1,7 +1,10 @@
 const num1 = document.getElementById('num1')! as HTMLInputElement;
 const num2 = document.getElementById('num2')! as HTMLInputElement;
 
-const add = (num1: number | string , num2: number | string, outputConversion: 'as-number' | 'as-text') => { 
+type Combine = number | string
+type Convert = 'as-number' | 'as-text'
+
+const add = (num1: Combine, num2: Combine, outputConversion: Convert) => { 
 	return typeof num1 === 'number' && typeof num2 === 'number' || outputConversion === 'as-number' ?  +num1 + +num2 : num1.toString() + num2.toString()
 }
 
