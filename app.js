26e4ca5d71ae2d9d6eed0ca5efd9fa14a7790316
savannah-yahsuanlin <savannahwang75@gmail.com>
2023-03-08 19:00:52 +0800

    literal types

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var num1 = document.getElementById('num1');
 var num2 = document.getElementById('num2');
-var add = function (num1, num2) {
-    return typeof num1 === 'number' && typeof num2 === 'number' ? num1 + num2 : num1.toString() + num2.toString();
+var add = function (num1, num2, outputConversion) {
+    return typeof num1 === 'number' && typeof num2 === 'number' || outputConversion === 'as-number' ? +num1 + +num2 : num1.toString() + num2.toString();
 };
-console.log(add(1, 3));
-console.log(add('moe', 'larry'));
+console.log(add(1, 3, 'as-number'));
+console.log(add('1', '3', 'as-number'));
+console.log(add('moe', 'larry', 'as-text'));
--- app.ts
+++ app.ts
@@ -1,9 +1,10 @@
 const num1 = document.getElementById('num1')! as HTMLInputElement;
 const num2 = document.getElementById('num2')! as HTMLInputElement;
 
-const add = (num1: number | string , num2: number | string) => { 
-	return typeof num1 === 'number' && typeof num2 === 'number' ?  num1 + num2 : num1.toString() + num2.toString()
+const add = (num1: number | string , num2: number | string, outputConversion: 'as-number' | 'as-text') => { 
+	return typeof num1 === 'number' && typeof num2 === 'number' || outputConversion === 'as-number' ?  +num1 + +num2 : num1.toString() + num2.toString()
 }
 
-console.log(add(1, 3))
-console.log(add('moe', 'larry'))
+console.log(add(1, 3, 'as-number'))
+console.log(add('1', '3', 'as-number'))
+console.log(add('moe', 'larry', 'as-text'))
